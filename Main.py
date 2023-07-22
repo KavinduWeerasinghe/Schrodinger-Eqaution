1b0f17e6f2ce699cd7f83bee3e578a986875cdc3
KavinduWeerasinghe <75959908+KavinduWeerasinghe@users.noreply.github.com>
2023-07-23 00:28:10 +0530

    basic-done

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 import scipy
 import numpy as np
-from Potentials import Infinity_well
+from Potentials import Infinity_well,finite_well,harmonic_well,potential_barrier
 import matplotlib.pyplot as plt
 
 def Hamiltonian(x,V,dx,m=1,h=1)->np.ndarray:
@@ -8,14 +8,14 @@ def Hamiltonian(x,V,dx,m=1,h=1)->np.ndarray:
                                   [-1, 0, 1],
                                   shape=(x.size, x.size))/dx**2
     H2=-(h**2/(2*m))*D_matrix+V
-    H=H2*(-1j)
+    H=H2
     return H
 
 def setup_psi(H,dx):
     eig_val,eig_vec=scipy.linalg.eigh(H)
-    prob=np.abs(eig_vec)**2
+    prob=(np.abs(eig_vec))**2
     prob/=np.sum(prob,axis=0)*dx
-    return prob[:,:10]
+    return prob[:,:5]
 
 def setup_Vector(V):
     V_matrix=np.diag(V)
@@ -26,22 +26,29 @@ def setup_base_vector(start=0,end=10,dx=0.01):
     return x,dx
 
 def plot_potential(x,V_x,lable=None):
-    axs.plot(x,V_x,label=lable)
+    axs.plot(x,V_x,label=lable,linestyle="--",linewidth=0.5)
 
 def plot_prob(x,psi,lable=None):
-    axs.plot(x,psi,label=lable)
+    L=len(psi[0])
+    M=max(psi[:,0])
+    for i in range(L):
+        M=max(psi[:,i])
+        y=psi[:,i]+i
+        lable_new=lable+" : n = "+str(i+1)
+        axs.plot(x,y,label=lable_new)
+    return (L+1)
 
 x,dx=setup_base_vector()
 x0 = 3.0  #center
-V_x=Infinity_well(x)
+V_x=potential_barrier(x)
 
 V=setup_Vector(V_x)
 H=Hamiltonian(x,V,dx)
 psi=setup_psi(H,dx)
 fig, axs = plt.subplots()
 plot_potential(x,V_x,lable="Potential")
-plot_prob(x,psi,lable="Probability")
+K=plot_prob(x,psi,lable="Probability")
 
-plt.ylim([0, 2*max(psi[:,0])])
+plt.ylim([0, K])
 axs.legend()
 plt.show()
--- Potentials.py
+++ Potentials.py
@@ -3,6 +3,21 @@ import numpy as np
 def Infinity_well(x,infinity_value=2000000000000):
     L=len(x)
     V_x=np.ones(L)*infinity_value
-    V_x[L//2-L//4:L//2+L//4]=0
+    V_x[L//2-L//10:L//2+L//10]=0
     return V_x
     
+def finite_well(x,finite_value=10):
+    L=len(x)
+    V_x=np.ones(L)*finite_value
+    V_x[L//2-L//10:L//2+L//10]=0
+    return V_x
+
+def harmonic_well(x,k=1):
+    V_x=k*(x-5)**2
+    return V_x
+
+def potential_barrier(x):
+    L=len(x)
+    V_x=np.ones(L)*0
+    V_x[L//2-L//10:L//2+L//10]=10
+    return V_x
